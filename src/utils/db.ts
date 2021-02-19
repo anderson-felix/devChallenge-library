61d8b105525a734a585380c9be2e2f36fb5235c1
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-19 00:11:34 -0300

    setup updated, clean warning messages

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 mongoose.connect(
   "mongodb+srv://library-challenge:hEkBYJmezeuL1ZRN@dark-labs.zpzhw.mongodb.net/library-challenge?retryWrites=true&w=majority",
   {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
